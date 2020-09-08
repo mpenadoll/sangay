c1a62cdc86c94ad58dc82bbbfb1e3d30bb50825c
mpenadoll <36230818+mpenadoll@users.noreply.github.com>
2020-09-07 19:04:37 -0700

    comments

--- sangay/settings.h
+++ sangay/settings.h
@@ -27,7 +27,7 @@ float pulseKp, pulseKi, pulseKd; // pulse conversion declarations
 // CONSTANTS
 const unsigned int sampleTime = 30; //sample time for derivative measurements [ms]
 const unsigned int debounceDelay = 50;  // the debounce time; increase if the output flickers
-const int error = 5; // error [pulses] allowable for position control
+const int error = 5; // error [pulses] allowable for position control. 5 pulses = 0.27 mm
 const unsigned int limitTime = 100; // time to move into the limit switch [ms]
 const int homeStep = 3; // distance to travel each homing step/loop
 const int lightPosition = 0.2 * stroke; // position to turn on lights [pulses]
