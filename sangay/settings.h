f4a6fdbd2bc4a6160542ba7e63f26d4f470a30c2
mpenadoll <36230818+mpenadoll@users.noreply.github.com>
2020-10-13 19:31:32 -0700

    settings (WIP)
    - testing

--- sangay/settings.h
+++ sangay/settings.h
@@ -4,8 +4,8 @@
 // Motion Profile Variables
 float strokeMM = 500.0; //* stroke [mm] 431.8
 float pulleyRadius = 24.41; // radius of the pulley [mm]
-float maxSpeedMM = 80.0; //* max speed of rack [mm/s]
-float accelMM = 150.0; //* acceleration of rack [mm/s^2]
+float maxSpeedMM = 40.0; //* max speed of rack [mm/s]
+float accelMM = 100.0; //* acceleration of rack [mm/s^2]
 float PPR = 1440.0; // number of pulses of encoder per rev
 
 // convert to [pulse] units. Note - gear ratio not included because encoder is on output shaft
@@ -17,18 +17,18 @@ float accel = PPR * 2 * accelMM / (pulleyRadius * 2 * 3.14 * 1000 * 1000);  // a
 
 // Set PID Controller Settings for Position Control
 float Kp = 7800.0; //* proportional gain [V / m]
-float Ki = 200.0; //* integral gain [V / (m*s)]
-float Kd = 0.0; //* derivative gain [V * s / m]
+float Ki = 0.0; //* integral gain [V / (m*s)]
+float Kd = 10.0; //* derivative gain [V * s / m]
 
 // CONSTANTS
 const unsigned int sampleTime = 30; // sample time for derivative measurements [ms]
 const unsigned int debounceDelay = 50;  // the debounce time; increase if the output flickers
-const int maxError = 5; //* error [pulses] allowable for position control. 5 pulses = 0.27 mm
+const int maxError = 10; //* error [pulses] allowable for position control. 5 pulses = 0.27 mm
 const int homeOffset = -20; // distance between limit and 0, positive. 20 pulses = 1.07 mm
 const long lightPosition = 0.2 * stroke; // position to turn on lights [pulses]
 const long supplyVoltage = 24000; // system voltage, long due to wrapping of milliVolts [mV]
 const float minDegenSpeed = 1; //* minimum speed that degen is effective. 1 pulses/ms = 53.5 mm/s [pulses / ms]
-const int minPWM = 255 * 1 / 24; //* minimum PWM value that motor will move [analog]
+const int minPWM = 255 * 0.0 / 24; //* minimum PWM value that motor will move [analog]
 const float minSpeed = 0.0001; // minimum speed at which the brake can be applied [pulses / ms]
 const int stopFudge = 20; //* fudge factor for stopping [pulses]
 
